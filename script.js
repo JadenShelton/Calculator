468adaf1d0757364237ae483e07aa3aee7dc9072
Jaden Shelton <jaden@sheltonz.co>
2026-06-05 16:15:26 -0600

    Calculator functions mostly

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let a = '0';
-let operator = ' ';
-let b = ' ';
+let operator = '';
+let number = '';
+let numbers = [];
 
 
 function add(a, b) {
@@ -21,13 +21,13 @@ function divide(a, b) {
 
 function operate(operator, a, b) {
     switch (operator) {
-        case ' + ':
+        case '+':
             return add(a, b);
-        case ' - ':
+        case '-':
             return subtract(a, b);
-        case ' * ':
+        case '*':
             return multiply(a, b);
-        case ' / ':
+        case '/':
             return divide(a, b);
         default:
             console.error(`${operator} is invalid as an operator!`);
@@ -35,31 +35,70 @@ function operate(operator, a, b) {
 }
 
 const operatorRow = document.getElementById('operatorRow');
-
 operatorRow.addEventListener('click', (event) => {
     if(event.target.tagName === 'BUTTON') {
+
+        if(number !== '') {
+            numbers.push(Number(number));
+            number = '';
+        }
+
+        if (numbers.length === 2 && operator !== '') {
+            calculate();
+        }
+
         const operation = event.target.id;
-    
         switch (operation) {
             case 'add':
-                operator = ' + ';
+                operator = '+';
                 break;
             case 'subtract':
-                operator = ' - ';
+                operator = '-';
                 break;
             case 'multiply':
-                operator = ' * ';
+                operator = '*';
                 break;
             case 'divide':
-                operator = ' / ';
+                operator = '/';
                 break;
             default:
-                operator = ' ';
+                operator = '';
         }
     }
-    console.log(operator);
+    console.log("Current operator:", operator);
 });
 
+const numRows = document.getElementsByClassName("numRow");
+for(let i = 0; i < numRows.length; ++i) {
+    numRows[i].addEventListener('click', (event) => {
+        if(event.target.tagName === 'BUTTON') {
+            number += event.target.textContent;
+            console.log("Current typing string", number);
+            document.getElementById("outPut").textContent = number;
+        }
+    });
+}
 
+const calc = document.getElementById('calc');
+calc.addEventListener('click', () => {
+    if (number !== '') {
+        numbers.push(Number(number))
+        number = '';
+    }
+    if (numbers.length === 2 && operator !== '') {
+        calculate();
+        operator = '';
+    }
+});
 
+function calculate () {
+    let b = numbers.pop();
+    let a = numbers.pop();
+    const result = operate(operator, a, b);
 
+    document.getElementById("outPut").textContent = result;
+    console.log(result);
+    
+    numbers.push(result);
+    console.table(numbers);
+}
